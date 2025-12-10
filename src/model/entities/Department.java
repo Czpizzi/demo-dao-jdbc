d38b8f80dfc64b9fa92fa7f1e5f5571349da9876
Luiz Pizzi <luizcpizzi@gmail.com>
2025-12-09 21:36:27 -0300

    findByDepartment implementation

--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -4,15 +4,14 @@ import java.io.Serializable;
 import java.util.Objects;
 
 public class Department implements Serializable {
-	
-	
+
 	private static final long serialVersionUID = 1L;
-	
+
 	private Integer id;
 	private String name;
-	
-	public Department( ) {
-		
+
+	public Department() {
+
 	}
 
 	public Department(Integer id, String name) {
@@ -57,6 +56,5 @@ public class Department implements Serializable {
 	public String toString() {
 		return "Department [id=" + id + ", name=" + name + "]";
 	}
-	
 
 }
